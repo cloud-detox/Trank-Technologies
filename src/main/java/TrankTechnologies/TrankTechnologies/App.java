d8a94f09c6618cdc651efecfd527ddab1f70a96d
NehaChaudhary12345 <167313901+NehaChaudhary12345@users.noreply.github.com>
2024-05-15 11:12:47 +0530

    first commit

---
+++ src/main/java/TrankTechnologies/TrankTechnologies/App.java
@@ -0,0 +1,13 @@
+package TrankTechnologies.TrankTechnologies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
